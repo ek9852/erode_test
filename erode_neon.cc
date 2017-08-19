4d0e1e7c4c9479349ba172aa29552ace28dfe4be
Keith Mok <ek9852@gmail.com>
2017-08-18 21:42:36 -0700

    Handle edge case for neon implementation
    Test and compare with Halide implementation
    and get identical result.

--- erode_neon.cc
+++ erode_neon.cc
@@ -2,14 +2,19 @@
 
 #include "erode_neon.h"
 #include <arm_neon.h>
+#include <assert.h>
 
-inline void
-erode3x3_neon_kernel(uint8_t *in_data, uint8_t *out_data, const int in_stride)
+static inline void __attribute__((always_inline))
+erode3x3_neon_kernel(uint8_t *in_data, uint8_t *out_data, const int in_stride,
+		const bool left, const bool top, const bool right, const bool bottom)
 {
-        uint8_t         *in_ptr   = in_data - 1;
-        const uint8x16_t top_data = vld1q_u8(in_ptr - in_stride);
+	// | 1 | .... | 8 | 8 |
+	const uint8x8_t max = vdup_n_u8(255);
+
+        uint8_t         *in_ptr   = left ? in_data : (right ? in_data - 8 : in_data - 1);
         const uint8x16_t mid_data = vld1q_u8(in_ptr); 
-        const uint8x16_t bot_data = vld1q_u8(in_ptr + in_stride);
+        const uint8x16_t top_data = top ? mid_data : vld1q_u8(in_ptr - in_stride);
+        const uint8x16_t bot_data = bottom ? mid_data : vld1q_u8(in_ptr + in_stride);
         
         uint8x8_t top_high_data = vget_high_u8(top_data);
         uint8x8_t top_low_data  = vget_low_u8(top_data);
@@ -22,31 +27,72 @@ erode3x3_neon_kernel(uint8_t *in_data, uint8_t *out_data, const int in_stride)
         
         uint8x8_t p0, p1;
         
-        p0 = top_low_data;
-        p1 = vext_u8(top_low_data, top_high_data, 1);
-        p0 = vmin_u8(p0, p1);
+	if (right) {
+		p0 = top_high_data;
+		p1 = vext_u8(top_low_data, top_high_data, 7);
+		p0 = vmin_u8(p0, p1);
+
+		p1 = vext_u8(max, top_high_data, 7);
+		p0 = vmin_u8(p0, p1);
+
+		p1 = mid_high_data;
+		p0 = vmin_u8(p0, p1);
+
+		p1 = vext_u8(mid_low_data, mid_high_data, 7);
+		p0 = vmin_u8(p0, p1);
+
+		p1 = vext_u8(max, mid_high_data, 7);
+		p0 = vmin_u8(p0, p1);
+
+		p1 = bot_high_data;
+		p0 = vmin_u8(p0, p1);
+
+		p1 = vext_u8(bot_low_data, bot_high_data, 7);
+		p0 = vmin_u8(p0, p1);
+
+		p1 = vext_u8(max, bot_high_data, 7);
+		p0 = vmin_u8(p0, p1);
+	} else {
+		p0 = top_low_data;
+		p1 = vext_u8(top_low_data, top_high_data, 1);
+		p0 = vmin_u8(p0, p1);
+
+		if (!left) {
+			p1 = vext_u8(top_low_data, top_high_data, 2);
+			p0 = vmin_u8(p0, p1);
+		} else {
+			p1 = vext_u8(max, top_low_data, 7);
+			p0 = vmin_u8(p0, p1);
+		}
         
-        p1 = vext_u8(top_low_data, top_high_data, 2);
-        p0 = vmin_u8(p0, p1);
-        
-        p1 = mid_low_data;
-        p0 = vmin_u8(p0, p1);
-        
-        p1 = vext_u8(mid_low_data, mid_high_data, 1);
-        p0 = vmin_u8(p0, p1);
-        
-        p1 = vext_u8(mid_low_data, mid_high_data, 2);
-        p0 = vmin_u8(p0, p1);
-        
-        p1 = bot_low_data;
-        p0 = vmin_u8(p0, p1);
-        
-        p1 = vext_u8(bot_low_data, bot_high_data, 1);
-        p0 = vmin_u8(p0, p1);
-        
-        p1 = vext_u8(bot_low_data, bot_high_data, 2);
-        p0 = vmin_u8(p0, p1);
-        
+		p1 = mid_low_data;
+		p0 = vmin_u8(p0, p1);
+
+		p1 = vext_u8(mid_low_data, mid_high_data, 1);
+		p0 = vmin_u8(p0, p1);
+
+		if (!left) {
+			p1 = vext_u8(mid_low_data, mid_high_data, 2);
+			p0 = vmin_u8(p0, p1);
+		} else {
+			p1 = vext_u8(max, mid_low_data, 7);
+			p0 = vmin_u8(p0, p1);
+		}
+
+		p1 = bot_low_data;
+		p0 = vmin_u8(p0, p1);
+
+		p1 = vext_u8(bot_low_data, bot_high_data, 1);
+		p0 = vmin_u8(p0, p1);
+
+		if (!left) {
+			p1 = vext_u8(bot_low_data, bot_high_data, 2);
+			p0 = vmin_u8(p0, p1);
+		} else {
+			p1 = vext_u8(max, bot_low_data, 7);
+			p0 = vmin_u8(p0, p1);
+		}
+        }
         vst1_u8(out_data, p0);
 }
 
@@ -54,15 +100,40 @@ void
 erode3x3_neon(uint8_t *in_data, uint8_t *out_data, int w, int h)
 {
 	int i, j;
-	// Ignore edge for the mean time
+
+	assert(w >= 16);
+	assert(h >= 2);
+
+	// top row
+	erode3x3_neon_kernel(in_data, out_data, w, true, true, false, false);
+	for (i = 1; i < w - 8; i+=8) {
+		erode3x3_neon_kernel(in_data+i, out_data+i, w, false, true, false, false);
+	}
+	// move backward last one, so that it align to 8 bytes
+	erode3x3_neon_kernel(in_data+w-8, out_data+w-8, w, false, true, true, false);
 	in_data += w;
 	out_data += w;
+
+	// middle row
 	for (j = 1; j < h - 1; j++) {
-		for (i = 1; i < w - 8; i+=8) {
-			erode3x3_neon_kernel(in_data+i, out_data+i, w);
+		erode3x3_neon_kernel(in_data, out_data, w, true, false, false, false);
+		for (i = 8; i < w - 8; i+=8) {
+			erode3x3_neon_kernel(in_data+i, out_data+i, w, false, false, false, false);
 		}
+		// move backward last one, so that it align to 8 bytes
+		erode3x3_neon_kernel(in_data+w-8, out_data+w-8, w, false, false, true, false);
 		in_data += w;
 		out_data += w;
 	}
+
+	// bottom row
+	erode3x3_neon_kernel(in_data, out_data, w, true, false, false, true);
+	for (i = 1; i < w - 8; i+=8) {
+		erode3x3_neon_kernel(in_data+i, out_data+i, w, false, false, false, true);
+	}
+	// move backward last one, so that it align to 8 bytes
+	erode3x3_neon_kernel(in_data+w-8, out_data+w-8, w, false, false, true, true);
+	in_data += w;
+	out_data += w;
 }
 #endif
